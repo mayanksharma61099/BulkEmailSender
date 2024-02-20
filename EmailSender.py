7767beddaf81bfe18232169dbde5d12108c56cf5
Mayank Sharma <61326177+mayanksharma61099@users.noreply.github.com>
2024-02-20 22:43:03 +0530

    Update EmailSender.py

--- EmailSender.py
+++ EmailSender.py
@@ -3,8 +3,8 @@ import pandas as pd
 import smtplib
 
 # Set your email address and password
-your_email = "mayanksharma61099@gmail.com"
-your_password = "gqsa gftv olbj vvlm"
+your_email = "your email"
+your_password = "app password"
 
 # Create a server object and log in
 server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -12,12 +12,12 @@ server.starttls()
 server.login(your_email, your_password)
 
 # Open the excel file and get the data frame
-path = "ProdData.xlsx"
+path = "TestData.xlsx"
 email_list = pd.read_excel(path)
 
 # Get the name and email ids from the data frame
-name_index = 5 # Change this to match your column index
-email_index = 6 # Change this to match your column index
+name_index = 1 # Change this to match your column index
+email_index = 2 # Change this to match your column index
 names = email_list.iloc[:, name_index]
 emails = email_list.iloc[:, email_index]
 
@@ -25,18 +25,11 @@ emails = email_list.iloc[:, email_index]
 for name, email in zip(names, emails):
     # Set the recipient, subject, and message 
     recipient = email
-    subject = "Thank you for your response."
+    subject = "Test Subject"
     message = f"""
 Hello {name},
 
-Thank you for giving your valuable inputs in Leveraging Customer Experience using AI with Embedded Finance form.
-
-This will really help me analyzing user experience and study the market for Embedded Finance.
-
-I appreciate your time and feedback.
-
-Best regards,
-Mayank Sharma
+Here comes the test content
 """
 
 
